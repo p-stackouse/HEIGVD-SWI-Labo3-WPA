f5f3c602339984610df17751a2abf2ab189e9975
root <guillaume.blanco@heig-vd.ch>
2019-05-01 09:34:25 -0400

    partie 1

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -42,13 +42,15 @@ wpa=rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
+ssid        =  wpa[0].info  #"SWI"
+APmac       =  wpa[1].addr3 #a2b_hex("cebcc8fdcab7")
+Clientmac   =  wpa[1].addr1 #a2b_hex("0013efd015bd")
+
+
 
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce      = str(wpa[5][Raw]).encode("HEX")[26:90] #a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+SNonce      = str(wpa[6][Raw]).encode("HEX")[26:90] # a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
@@ -62,10 +64,11 @@ print "\n\nValues used to derivate keys"
 print "============================"
 print "Passphrase: ",passPhrase,"\n"
 print "SSID: ",ssid,"\n"
-print "AP Mac: ",b2a_hex(APmac),"\n"
-print "CLient Mac: ",b2a_hex(Clientmac),"\n"
-print "AP Nonce: ",b2a_hex(ANonce),"\n"
-print "Client Nonce: ",b2a_hex(SNonce),"\n"
+print "AP Mac: ",APmac,"\n"
+print "CLient Mac: ",Clientmac,"\n"
+
+print "AP Nonce: ", ANonce,"\n"#b2a_hex(ANonce),"\n"
+print "Client Nonce: ",SNonce,"\n"#b2a_hex(SNonce),"\n"
 
 #calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
 pmk = pbkdf2_hex(passPhrase, ssid, 4096, 32)
